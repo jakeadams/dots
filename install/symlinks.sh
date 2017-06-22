d100a9c59784005e27248c3b9cb58cf2edc7a08b
Jake Adams <jsdev42+github@gmail.com>
2017-06-22 07:34:30 -0400

    add symlink script

---
+++ install/symlinks.sh
@@ -0,0 +1,8 @@
+ln -s ~/stash/local-dotfiles/41-functions-local .zshrc.d/41-functions-local
+ln -s ~/stash/local-dotfiles/51-path-local .zshrc.d/51-path-local
+ln -s ~/stash/local-dotfiles/61-env-local .zshrc.d/61-env-local
+ln -s ~/stash/local-dotfiles/81-aliases-local .zshrc.d/81-aliases-local
+
+ln -s ~/stash/local-dotfiles/brew-local install/brew-local
+
+ln -s ~/stash/local-dotfiles/gitconfig-shared-local git/gitconfig-shared-local
